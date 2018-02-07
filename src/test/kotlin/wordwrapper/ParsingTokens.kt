280b2a6da4433c508d4167a40474415cd38ae651
Paul D'Ambra <paul.dambra@coopdigital.co.uk>
2018-02-07 09:24:52 +0000

    can vary the number of columns

--- src/test/kotlin/wordwrapper/ParsingTokens.kt
+++ src/test/kotlin/wordwrapper/ParsingTokens.kt
@@ -12,14 +12,20 @@ object ParsingTokens : Spek(
         val rows = wrap(tokenStream)
         assert(rows).containsAll("the mad", "cat has", "no", "pajamas")
     }
+
+    it("can split on 8 columns") {
+        val tokenStream = listOf("the", "crazy", "cat", "has", "no", "pajamas").asSequence()
+        val rows = wrap(tokenStream, 9)
+        assert(rows).containsAll("the crazy", "cat has", "no", "pajamas")
+    }
 })
 
-fun wrap(tokens: Sequence<String>): List<String> {
+fun wrap(tokens: Sequence<String>, columns: Int = 7): List<String> {
     val wrapped = mutableListOf<String>()
     var currentRow = ""
     tokens.forEach {
         val candidate = "$currentRow $it".trim()
-        currentRow = if (candidate.length > 7) {
+        currentRow = if (candidate.length > columns) {
             wrapped.add(currentRow)
             it
         } else {
