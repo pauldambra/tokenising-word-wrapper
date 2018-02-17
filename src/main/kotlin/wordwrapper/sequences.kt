fb09ac992f1bf29ea9819956887375b8598b68c1
Paul D'Ambra <paul.dambra@coopdigital.co.uk>
2018-02-17 20:00:33 +0000

    refactored so that splitter could be passed in

--- src/main/kotlin/wordwrapper/sequences.kt
+++ src/main/kotlin/wordwrapper/sequences.kt
@@ -1,10 +1,16 @@
 package wordwrapper
 
+import wordwrapper.Tokenise.Companion.byWord
 import kotlin.coroutines.experimental.buildSequence
 
-fun tokenise(input: String) = buildSequence {
-    val splat = input.split("\\s+".toRegex())
-    splat.forEach { yield(it) }
+class Tokenise {
+    companion object {
+        fun byWord(s: String) = s.split("\\s+".toRegex())
+    }
+}
+
+fun tokenise(input: String, splitter: (s: String) -> List<String> = ::byWord) = buildSequence {
+    splitter(input).forEach { yield(it) }
 }
 
 fun wrap(tokens: Sequence<String>, columns: Int = 7) = buildSequence {
--- src/test/kotlin/wordwrapper/TheTokeniser.kt
+++ src/test/kotlin/wordwrapper/TheTokeniser.kt
@@ -5,6 +5,7 @@ import assertk.assertions.isEqualTo
 import org.jetbrains.spek.api.Spek
 import org.jetbrains.spek.api.dsl.context
 import org.jetbrains.spek.api.dsl.it
+import wordwrapper.Tokenise.Companion.byWord
 
 object TheTokeniser : Spek(
 {
@@ -12,6 +13,13 @@ object TheTokeniser : Spek(
         it("can tokenise to sequence by word boundary") {
             val input = "the mad cat has no pajamas"
             val tokenisedStream = tokenise(input)
+            val expectedTokenStream = sequenceOf("the", "mad", "cat", "has", "no", "pajamas")
+            assert(tokenisedStream.toList()).isEqualTo(expectedTokenStream.toList())
+        }
+
+        it("can explicitly tokenise by word boundary") {
+            val input = "the mad cat has no pajamas"
+            val tokenisedStream = tokenise(input, ::byWord)
             val expectedTokenStream = sequenceOf("the", "mad", "cat", "has", "no", "pajamas")
             assert(tokenisedStream.toList()).isEqualTo(expectedTokenStream.toList())
         }
